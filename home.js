e600d20911655866317402e789d23c59edd9f264
pedrooliveira <futr3pontocom@hotmail.com>
2023-01-03 12:46:50 +0000

    Nova home finalizada

--- home.js
+++ home.js
@@ -1,3 +1,7 @@
 function mostraNome(nome) {
     print(nome);
 }
+
+function mostraSobreNome(sobrenome) {
+    return sobrenome;
+}
--- login.js
+++ login.js
@@ -1,3 +1,11 @@
 //Trabalhando no login
 
 //Finaliznaod sistems de lohin
+
+function login(nome, email){
+    return 'Bem vindo';
+}
+
+//Seja bem vindo
+
+//Sistems de login completo
